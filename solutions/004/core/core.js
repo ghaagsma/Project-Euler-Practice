763b841f1bbf1606df4a009ef27926866d8bc199
Jerry Haagsma <haagsmag@gmail.com>
2016-11-23 13:05:51 -0500

    -Optimized solution 004

--- solutions/004/core/core.js
+++ solutions/004/core/core.js
@@ -8,10 +8,10 @@
             lowerLimit = Math.pow(10, digits - 1),
             result = 0;
         for (let i = upperLimit - 1; i >= lowerLimit; --i) {
-            for (let j = upperLimit - 1; j >= lowerLimit; --j) {
+            for (let j = i; j >= lowerLimit; --j) {
                 let product = i * j;
-                if (stringUtilities.isPalindrome(product.toString()) &&
-                    product > result) {
+                if (product > result &&
+                    stringUtilities.isPalindrome(product.toString())) {
                     result = product;
                 }
             }
