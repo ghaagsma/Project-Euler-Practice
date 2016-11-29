62bc51a1f8af3cdb526ebe07c04210a9f8daf981
Jerry Haagsma <haagsmag@gmail.com>
2016-11-28 21:50:21 -0500

    -Added solution 008

---
+++ solutions/008/index.js
@@ -0,0 +1,20 @@
+(() => {
+    'use strict';
+
+    let now = require('performance-now'),
+        inputParser = require('euler-common/src/input-parser'),
+        core = require('./core/core');
+
+    let startTime = now();
+
+    const defaultTarget = 13;
+
+    let input = process.argv[2],
+        target = inputParser.isInteger(input) ? parseInt(input) : defaultTarget,
+        result = core.getMaxProduct(target);
+
+    console.log(`The answer is ${result}`);
+
+    let duration = (now() - startTime).toFixed(3);
+    console.log(`Execution time: ${duration}ms`);
+})();
