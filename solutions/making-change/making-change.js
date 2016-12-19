3775b3e07cfa0cf3600f37f867d462d72f4f452f
Jerry Haagsma <haagsmag@gmail.com>
2016-12-18 17:57:17 -0800

    -Added making-change solution

---
+++ solutions/making-change/making-change.js
@@ -0,0 +1,21 @@
+(() => {
+    'use strict';
+
+    let makeChange = (amount, denominations) => {
+        let m = denominations.length,
+            results = [];
+
+        results[0] = 1;
+        for (let i = 1; i <= amount; ++i)
+            results[i] = 0;
+
+        denominations.map((coin) => {
+            for (let i = coin; i <= amount; ++i)
+                results[i] += results[i - coin];
+        });
+
+        return results[amount];
+    };
+
+    module.exports = makeChange;
+})();
