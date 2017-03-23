a2924e2ae1b992de4863e55544310582b6b67d52
Jerry Haagsma <haagsmag@gmail.com>
2017-03-22 23:08:46 -0400

    -Updated solution for highest-product-of-3

--- solutions/highest-product-of-3/highest-product-of-3.js
+++ solutions/highest-product-of-3/highest-product-of-3.js
@@ -5,12 +5,12 @@
         if (!ints || ints.length < 3) {
             throw new Error('Expected array of length 3 or more.');
         }
-        let max = ints[0],
-            min = max,
-            hp2 = 0,
-            lp2 = 0,
-            hp3 = 0;
-        for (let i = 1; i < ints.length; ++i) {
+        let max = Math.max(ints[0], ints[1]),
+            min = Math.min(ints[0], ints[1]),
+            hp2 = ints[0] * ints[1],
+            lp2 = hp2,
+            hp3 = hp2 * ints[2];
+        for (let i = 2; i < ints.length; ++i) {
             hp3 = Math.max(hp3, hp2 * ints[i], lp2 * ints[i]);
             hp2 = Math.max(hp2, max * ints[i], min * ints[i]);
             lp2 = Math.min(lp2, max * ints[i], min * ints[i]);
